3d23d7a83903ec485bb41e89f95277114bd774f4
Kishor M <kishwormagranti.11@gmail.com>
2026-05-31 13:01:26 +0545

    Clean up README

--- TaskManager/obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
+++ TaskManager/obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0990de00b97733cbfca2ccade15be69ab1dd6c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
